bb8ece00ff6b234ac4363ec7288bbbbc0b46fae9
Neeraj Kumar <nijjukr@gmail.com>
2021-09-18 15:48:35 +0530

    Changed gitignore

---
+++ build/precache-manifest.000dc995200758f5b7ddf3dbad4708e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a3efee52912edaadcd",
+    "url": "/static/css/main.0d9765a9.chunk.css"
+  },
+  {
+    "revision": "f1a3efee52912edaadcd",
+    "url": "/static/js/main.d7cbf5b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee347db5b4be809d5e7b",
+    "url": "/static/js/2.ed802812.chunk.js"
+  },
+  {
+    "revision": "bde59e328d83e3baf8591f2c02230e11",
+    "url": "/index.html"
+  }
+];
